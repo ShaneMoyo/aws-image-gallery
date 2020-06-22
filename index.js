1d90093ce9f48a4e0bb8ab28c78a9a42064b08b8
shanemoyo <smoyo@pdx.edu>
2020-06-22 16:00:47 -0700

    working.

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
-require('dotenv/config'); 
-const express = require('express'); 
-const multer = require('multer'); 
+require('dotenv/config')
 
-const app = express(); 
-const port = 3000; 
+const express = require('express')
+const multer = require('multer')
+const AWS = require('aws-sdk')
+const { uuid } = require('uuidv4');
+
+const app = express()
+const port = 3000
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET
+})
 
 const storage = multer.memoryStorage({
-    destination: function(req, file, callback) { 
-        callback(null, ''); 
+    destination: function(req, file, callback) {
+        callback(null, '')
     }
-}); 
+})
 
-const upload = multer({ storage }).single('image'); 
+const upload = multer({storage}).single('image')
 
+app.post('/upload',upload,(req, res) => {
 
-app.post('/upload', upload, (req, res) => {
-    
-    console.log(req.file)
-    res.send({
-        message: "Hello World"
+    let myFile = req.file.originalname.split(".")
+    const fileType = myFile[myFile.length - 1]
+
+    const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: `${uuid()}.${fileType}`,
+        Body: req.file.buffer
+    }
+
+    s3.upload(params, (error, data) => {
+        if(error){
+            res.status(500).send(error)
+        }
+
+        res.status(200).send(data)
     })
-}); 
+})
 
-app.listen(port, () => { 
-    console.log(`server is up at ${port}`)
-}); 
+app.listen(port, () => {
+    console.log(`Server is up at ${port}`)
+})
