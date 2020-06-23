14ddcd9e0548dd6baf389450d376a3d17a1a6de7
shanemoyo <smoyo@pdx.edu>
2020-06-23 11:00:15 -0700

    fetching images.

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, {useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <img style={{ width: "200px", hight: "200px"}} src="https://shanebucket111.s3.us-east-2.amazonaws.com/130f7884-363a-440b-8b04-f87fbd2f5288.JPG"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require('dotenv/config')
+require('dotenv/config');
 
-const express = require('express')
-const multer = require('multer')
-const AWS = require('aws-sdk')
+const express = require('express');
+const multer = require('multer');
+const AWS = require('aws-sdk');
 const { uuid } = require('uuidv4');
 
 const app = express()
